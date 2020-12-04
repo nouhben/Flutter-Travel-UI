16caf3fec27b7671359c6a70c5b214b81e3a387c
nouh ben kadi <benkadi.nouh@icloud.com>
2020-12-04 15:25:25 +0100

    Text Field

--- lib/screens/hom/components/body.dart
+++ lib/screens/hom/components/body.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
+import '../../../shared/size_config.dart';
+import '../../../shared/size_config.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Image.asset(
-          'assets/images/home_bg.png',
-          fit: BoxFit.cover,
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(
+              'assets/images/home_bg.png',
+              height: getProportionateScreenWidth(315),
+              fit: BoxFit.cover,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                VerticalSpacing(of: 80),
+                Text(
+                  'Travelers',
+                  style: Theme.of(context).textTheme.headline2.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        height: 0.5,
+                      ),
+                ),
+                Text(
+                  'Travel Community App',
+                  style: Theme.of(context).textTheme.bodyText1.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ],
+            ),
+          ],
         ),
       ],
     );
--- lib/screens/hom/home_screen.dart
+++ lib/screens/hom/home_screen.dart
@@ -2,11 +2,13 @@ import 'package:animation_day_night/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../../shared/size_config.dart';
 import 'components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       extendBodyBehindAppBar: true, //to allow items to go behind the app bar
       appBar: AppBar(
